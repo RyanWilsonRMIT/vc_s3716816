280c292a101b37c75e92e03b03a5e4697cd961cf
ryan wilson <s3716816@student.rmit.edu.au>
2021-09-04 00:11:37 +1000

    Finished implementing functionality for input comp

--- src/components/input.js
+++ src/components/input.js
@@ -8,21 +8,37 @@ class Input extends React.Component {
       label: null,
       id: null,
       validation: null,
+      correct:false,
+      type:null,
+      errors:null,
     }
   }
   componentDidMount(){
-    let validation, errmsg;
+    let validation, errmsg, type;
     if (this.props.validation){
-      validation=this.props.validation;
-      errmsg = this.props.errmsg;
+      validation= this.props.validation;
     }
     else{
-      validation="any";
+      validation = [
+        {
+          rule: new RegExp("^.+$"),
+          msg: "Please Enter something",
+        },
+      ]
+    }
+    if (this.props.type){
+      type=this.props.type;
+    }
+    else{
+      type="text";
     }
     this.setState({
       label: this.props.label,
       id: this.props.id,
       validation: validation,
+      type:type,
+      errors:[],
+      correct:false,
     })
   }
   focused = () =>{
@@ -31,15 +47,39 @@ class Input extends React.Component {
   blured = () =>{
     this.setState({active:false});
   }
-  keyDown = (e)=>{
+  keyUp = (e)=>{
     //does e.target.data match the validation in this.state.validation
+    let validation = this.state.validation;
+    let data = e.target.value;
+    console.log(data)
+    let errors=[]
+    let correct=true
+    console.log(validation);
+    for (var v in validation){
+      console.log("Checking rule: " + validation[v].msg)
+      if (!data.match(validation[v].rule)){
+        console.log("failed");
+        correct=false;
+        errors.push(validation[v].msg)
+      }
+    }
+    this.setState({correct:correct,errors:errors})
+    
+    
   }
   render() {
+    let errorBody=[];
+    if (!this.state.correct){
+      let errors = this.state.errors;
+      for (var error in errors){
+        errorBody.push(<p>{errors[error]}</p>)
+      }
+    }
     return (
       <div>
         <p>{this.props.label + ": "}</p>
-        <input className="input" id = {this.props.id} placeholder = {this.props.label}></input>
-        <p>{this.state.validation}</p>
+        <input className={`input ${this.state.correct ? " correct" : " incorrect"}`} id = {this.props.id} placeholder = {this.props.label} onKeyUp={this.keyUp} type={this.state.type}></input>
+        {errorBody}
       </div>
     );
   }
--- src/components/loginRegister.js
+++ src/components/loginRegister.js
@@ -19,25 +19,53 @@ class LoginRegister extends React.Component {
   }
   render() {
     let selected = this.state.selected
+    let passwordValidation=[
+      {
+        rule: new RegExp("^.{6,}$"),
+        msg: "Your password must have at least 6 characters",
+      },
+      {
+        rule: new RegExp("^.*[a-z].*$"),
+        msg: "Your password must have at least 1 lowecase character",
+      },
+      {
+        rule: new RegExp("^.*[A-Z].*$"),
+        msg: "Your password must have at least 1 uppercase character",
+      },
+      {
+        rule: new RegExp("^.*[0-9].*$"),
+        msg: "Your password must have at least 1 number",
+      },
+      {
+        rule: new RegExp(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,@#.\/]/),
+        msg: "Your password must have at least 1 symbol",
+      },
+    ]
+    let emailValidation = [
+      {
+        rule:new RegExp(/^[a-zA-Z][a-zA-Z0-9\.]*@[a-zA-Z]*\.([\.a-zA-Z]*)*[a-zA-Z]$/),
+        msg:"Please enter a valid email address",
+      },
+    ]
     return (
       <div className="loginRegister">
         <div className="toggleSwitch">
-          <button className={`toggle ${(selected=="login") ? "selected": ""}`} onClick={this.swapLogin}>Log In</button>
-          <button className={`toggle ${(selected=="register") ? "selected": ""}`} onClick={this.swapRegister}>Register</button>  
+          <button className={`toggle ${(selected==="login") ? "selected": ""}`} onClick={this.swapLogin}>Log In</button>
+          <button className={`toggle ${(selected==="register") ? "selected": ""}`} onClick={this.swapRegister}>Register</button>  
         </div>
-        {selected=="login" &&
+        {selected==="login" &&
           <div className="login">
             <Input label="Username" id="username"></Input>
-            <Input label="Password" id="password" validation=".^6" errmsg = "Your password must be at least 6 characters and contain one number, one lowercase and uppercase letter, and !-=;'" type="password"></Input>
+            <Input label="Password" id="password" validation={passwordValidation} type="password"></Input>
             <input type="submit"></input>
           </div>
 
         }
-        {selected=="register" &&
+        {selected==="register" &&
           <div className="register">
-            <Input label="Email" id="email" validation=""></Input>
-            <Input label="Username" id="username" validation=""></Input>
-            <Input label="Password" id="password" validation=".^6" errmsg = "Your password must be at least 6 characters and contain one number, one lowercase and uppercase letter, and !-=;'" type="password"></Input>
+            <Input label="Email" id="email" validation={emailValidation}></Input>
+            <Input label="Username" id="username"></Input>
+            <Input label="Password" id="password" validation={passwordValidation} type="password"></Input>
             <input type="submit"></input>
           </div>
         }
