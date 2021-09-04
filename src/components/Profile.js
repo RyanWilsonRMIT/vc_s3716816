7b303d9e2bb1aeeca3435b12e8dc6b2133281f8b
ryan wilson <s3716816@student.rmit.edu.au>
2021-09-04 22:00:30 +1000

    added joinDate to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,7 @@ class Profile extends React.Component {
     this.state={
       username: null,
       email:null,
-      joinDate:null,
+      joinDate:new Date(),
     }
   }
   componentDidMount(){
@@ -14,24 +14,33 @@ class Profile extends React.Component {
     let profiles = JSON.parse(localStorage.getItem("accounts"))
     let username = localStorage.getItem("username")
     let profile={};
-    console.log(profiles)
-    console.log(username)
     for (let a in profiles){
       if (profiles[a].username===username){
         profile=profiles[a]
       }
     }
     //Adding the fields to state
+    profile.joinDate = new Date(Date.parse(profile.joinDate))
+    console.log(profile.joinDate.getFullYear());
     this.setState({
       username: profile.username,
       email: profile.email,
+      joinDate: profile.joinDate,
     })
   }
   render() {
+    let niceDate = "";
+    let date = this.state.joinDate
+    niceDate += date.getDate()
+    niceDate += "-"
+    niceDate += date.getMonth()+1
+    niceDate += "-"
+    niceDate += date.getFullYear()
     return (
       <div className="loginRegister">
         <h1>{this.state.username}</h1>
         <p>{this.state.email}</p>
+        <p>Has been a member since {niceDate}</p>
       </div>
     )
   }
